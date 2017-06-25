91fbbb43ac0443b40206a9222c192f7f4298aad2
juvega <cevegaju@hotmail.com>
2017-06-25 09:35:53 -0500

    Unit test con mock data.

---
+++ Cibertec.Mocked/MockedUnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Cibertec.Models;
+using Cibertec.UnitOfWork;
+using Moq;
+using Ploeh.AutoFixture;
+using System.Linq;
+
+namespace Cibertec.Mocked
+{
+    public static class MockedUnitOfWork
+    {
+        public static IUnitOfWork GetUnitOfWork()
+        {
+            Mock<IUnitOfWork> unit = new Mock<IUnitOfWork>();
+            unit.ConfigureCustomer();
+            return unit.Object;
+        }
+    }
+
+    public static class MockedUnitOfWorkExtensions
+    {
+        public static Mock<IUnitOfWork> ConfigureCustomer(this Mock<IUnitOfWork> mock)
+        {
+            var fixture = new Fixture();
+            var customerId = 0;
+            var customerList = fixture.CreateMany<Customer>(25).ToList();
+            customerList.ForEach(x => 
+            {
+                x.Id = customerId++;
+                x.FirstName = $"FirstName{customerId}";
+                x.LastName = $"LastName{customerId}";
+            });
+            mock.Setup(c => c.Customers.GetEntityById(It.IsAny<int>())).Returns(
+                (int id) => 
+                {
+                    return customerList.FirstOrDefault(x => x.Id == id);
+                });
+
+            mock.Setup(c => c.Customers.GetAll()).Returns(customerList);
+            mock.Setup(c => c.Customers.Insert(It.IsAny<Customer>())).Returns(1);
+            mock.Setup(c => c.Customers.Update(It.IsAny<Customer>())).Returns(true);
+            mock.Setup(c => c.Customers.Delete(It.IsAny<Customer>())).Returns(true);
+            mock.Setup(c => c.Customers.SearchByNames(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns((string firstName, string lastName) =>
+                {
+                    return customerList.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+                });
+            return mock;
+        }
+    }
+}
--- Cibertec.WebApi.Tests/CustomerControllerTests.cs
+++ Cibertec.WebApi.Tests/CustomerControllerTests.cs
@@ -1,7 +1,9 @@
-﻿using Cibertec.UnitOfWork;
+﻿using Cibertec.Mocked;
+using Cibertec.Models;
 using Cibertec.WebApi.Controllers;
-using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http.Results;
 using Xunit;
 
 namespace Cibertec.WebApi.Tests
@@ -11,14 +13,23 @@ namespace Cibertec.WebApi.Tests
         private readonly CustomerController _controller;
         public CustomerControllerTests()
         {
-            _controller = new CustomerController(new CibertecUnitOfWork());
+            _controller = new CustomerController(MockedUnitOfWork.GetUnitOfWork());
         }
                 
         [Fact]
         public void Customer_Get_By_Id()
         {
-            var result = _controller.Get(1);
+            var result = _controller.Get(1) as OkNegotiatedContentResult<Customer>;
             Assert.NotNull(result);
+            Assert.True(result.Content.Id == 1);
+        }
+
+        [Fact]
+        public void Customer_Get_All()
+        {
+            var result = _controller.GetList() as OkNegotiatedContentResult<IEnumerable<Customer>>;
+            Assert.NotNull(result);
+            Assert.True(result.Content.Count()==25);
         }
     }
 }
