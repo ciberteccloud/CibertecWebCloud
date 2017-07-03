d0d3872777ef4e3e48f753fcc9771e725280c215
Cesar Velarde <juvega@gmail.com>
2017-07-03 17:46:31 -0500

    Configuración inicial del proyecto.

---
+++ Cibertec.PartnerSalesProcessor/ProcessSale.cs
@@ -0,0 +1,76 @@
+﻿using Cibertec.Models;
+using Cibertec.UnitOfWork;
+using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+
+namespace Cibertec.PartnerSalesProcessor
+{
+    public class ProcessSale
+    {
+        private readonly IUnitOfWork _unit;
+        public ProcessSale(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+        public void ReadExcel()
+        {
+            string fileName = "PartnerSale.xlsx";
+            var workbook = new XLWorkbook(fileName);
+            workbook.CalculateMode = XLCalculateMode.Auto;
+
+            Customer customer = new Customer();
+            List<Order> orders = new List<Order>();
+            List<OrderItem> orderItems = new List<OrderItem>();
+
+            if (workbook.Worksheet(1).Name == "Customer")
+            {
+                var row = workbook.Worksheet(1).Row(2);
+                customer = new Customer
+                {
+                    FirstName = row.Cell("B").GetString(),
+                    LastName = row.Cell("C").GetString(),
+                    City = row.Cell("D").GetString(),
+                    Country = row.Cell("E").GetString(),
+                    Phone = row.Cell("F").GetString()
+                };
+                //_unit.Customers.Insert(customer);
+            }
+
+            if (workbook.Worksheet(2).Name == "Order")
+            {
+                int index = 2;
+                while (!workbook.Worksheet(2).Row(index).IsEmpty())
+                {
+                    var row = workbook.Worksheet(2).Row(index);
+                    orders.Add(new Order
+                    {
+                        CustomerId = customer.Id,
+                        OrderDate = row.Cell("B").GetDateTime(),
+                        OrderNumber = row.Cell("C").GetString(),
+                        TotalAmount = Convert.ToDecimal(row.Cell("E").ValueCached)
+                    });
+                    index++;
+                }
+            }
+
+            if (workbook.Worksheet(3).Name == "OrderItem")
+            {
+                int index = 2;
+                while (!workbook.Worksheet(3).Row(index).IsEmpty())
+                {
+                    var row = workbook.Worksheet(3).Row(index);
+                    orderItems.Add(new OrderItem
+                    {
+                        OrderId = row.Cell("B").GetValue<int>(),
+                        ProductId= row.Cell("C").GetValue<int>(),
+                        UnitPrice= Convert.ToDecimal(row.Cell("D").ValueCached),
+                        Quantity= row.Cell("E").GetValue<int>()
+                    });
+                    index++;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Cibertec.PartnerSalesProcessor/Program.cs
@@ -0,0 +1,18 @@
+﻿using Cibertec.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cibertec.PartnerSalesProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ProcessSale(new CibertecUnitOfWork());
+            service.ReadExcel();
+        }
+    }
+}
